bb5acc724a2ef05f3a4e0710f5363ca8e83ccc81
WaWeNoel <122537662+WaWeNoel@users.noreply.github.com>
2024-01-27 15:58:30 +0100

    Update file.js

--- file.js
+++ file.js
@@ -1,9 +1,13 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const { exec } = require('child_process');
 
 let mainWindow;
 
 function createWindow() {
+  // Eltávolítjuk az alapértelmezett menüt
+  Menu.setApplicationMenu(null);
+
   // Konfiguráljuk az ablakot
   mainWindow = new BrowserWindow({
     width: 800,
@@ -11,9 +15,8 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true
     },
-    frame: false, // Keret eltávolítása
-    titleBarStyle: 'hiddenInset', // Felső sáv elrejtése (titleBarStyle)
-    icon: path.join(__dirname, 'icon.png') // A saját ikonod elérési útja
+    frame: true, // Ablakkeret bekapcsolása
+    icon: path.join(__dirname, 'your-icon.png') // A saját ikonod elérési útja
   });
 
   // Betöltjük az üdvözlő HTML-t
@@ -26,7 +29,32 @@ function createWindow() {
 }
 
 // App események figyelése
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createWindow();
+
+  // Az updater.js fájl elérési útja
+  const updaterPath = path.join(__dirname, 'updater.js');
+
+  // Indítjuk az updater.js-t
+  const updaterProcess = exec(`node ${updaterPath}`, (error) => {
+    if (error) {
+      console.error(`Hiba az updater.js elindítása során: ${error}`);
+    }
+  });
+
+  // Figyeljük az updater.js kimenetét
+  updaterProcess.stdout.on('data', (data) => {
+    console.log(`Updater.js kimenet: ${data}`);
+  });
+
+  updaterProcess.stderr.on('data', (data) => {
+    console.error(`Updater.js hiba: ${data}`);
+  });
+
+  updaterProcess.on('close', (code) => {
+    console.log(`Updater.js leállt a kóddal: ${code}`);
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
